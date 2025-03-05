23422c9785b0acdff32aca4cde47a0d781bfbf73
Yoo-JiHyun <luhyun26@gmail.com>
2025-03-05 15:45:52 +0900

    2025.03.04

---
+++ JavaEdu/src/Chapter01/chap01.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Chapter01;
+
+/**
+ * 
+ */
+public class chap01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hi~!");
+	}
+
+}
